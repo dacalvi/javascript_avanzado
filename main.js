c670888ec41793c3b59e948e0d99943afe66e17e
u596328 <JAgu@proveedor.teco.com.ar>
2018-08-23 21:58:03 -0300

    version Inicial

---
+++ main.js
@@ -0,0 +1,8 @@
+function App(ID, nombre) {
+    var ID = ID;
+        function Usuario(nombre) {
+            let nombre = nombre;
+            return nombre;
+    }
+    return `ID: ${ID}, Usuario: ${nombre}` 
+}
